4fd7ab3c23bfd97f8f5025f3e2feca7c1c917a4e
Endre Sándor Varga <endre.varga@drewhk.com>
2013-05-21 14:47:39 +0200

    Added comments and cleaned up logging

--- app/com/typesafe/activator/supervision/ArithmeticService.scala
+++ app/com/typesafe/activator/supervision/ArithmeticService.scala
@@ -7,31 +7,62 @@ import akka.actor.{ActorSystem, Props, OneForOneStrategy, Actor}
 import akka.actor.SupervisorStrategy.{Restart, Stop, Escalate}
 import scala.collection.immutable._
 import scala.concurrent.forkjoin.ThreadLocalRandom
+import akka.event.Logging
+import scala.util.control.NonFatal
 
+// Represents an arithmetic expression involving integer numbers
 trait Expression {
   def left: Expression
   def right: Expression
 }
 
-case class Add(left: Expression, right: Expression) extends Expression
-case class Multiply(left: Expression, right: Expression) extends Expression
-case class Divide(left: Expression, right: Expression) extends Expression
+// Basic arithmetic operations that are supported by the ArithmeticService. Every operation except the constant value
+// has a left and right side. For example the addition in (3 * 2) + (6 * 6) has the left side (3 * 2) and the right
+// side (6 * 6).
+case class Add(left: Expression, right: Expression) extends Expression{
+  override val toString = s"($left + $right)"
+}
+case class Multiply(left: Expression, right: Expression) extends Expression {
+  override val toString = s"($left * $right)"
+}
+case class Divide(left: Expression, right: Expression) extends Expression {
+  override val toString = s"($left / $right)"
+}
 case class Const(value: Int) extends Expression {
   def left = this
   def right = this
+  override val toString = String.valueOf(value)
 }
 
-
+// A very simple service that accepts arithmetic expressions and tries to evaluate them. Since the calculation is
+// dangerous (at least for the sake of this example) it is delegated to a worker actor of type FlakyExpressionCalculator.
 class ArithmeticService extends Actor {
-  override val supervisorStrategy = OneForOneStrategy() {
-    case _: FlakynessException => Restart
-    case _ => Stop
+  val log = Logging(context.system, this)
+  // We are customizing the supervisor strategy of the service here. We set loggingEnabled to false, since we want
+  // to have custom logging.
+  // Our strategy here is to restart the child when a recoverable error is detected (in our case the dummy
+  // FlakynessException), but when arithmetic errors happen -- like division by zero -- we have no hope to recover
+  // therefore we stop the worker.
+  override val supervisorStrategy = OneForOneStrategy(loggingEnabled = false) {
+    case _: FlakynessException =>
+      log.warning("Evaluation of a top level expression failed, restarting.")
+      Restart
+    case e: ArithmeticException =>
+      log.error("Evaluation failed beause of: {}", e.getMessage)
+      Stop
+    // It is a good practice to use scala.util.control.NonFatal to not match on dangerous exceptions. See the
+    // ScalaDocs for details on what this does.
+    case NonFatal(e) =>
+      log.error(e, "Unexpected failure.")
+      Stop
   }
 
   def receive = {
     case e: Expression =>
+      // We delegate the dangerous task of calculation to a worker, passing the expression as a constructor argument
+      // to the actor.
       context.actorOf(FlakyExpressionCalculator(e, Left))
-    case Result(value, _) => println("Got result: " + value)
+    case Result(expr, value, _) => log.info("Result {} = {}", expr, value)
   }
 
 }
@@ -39,26 +70,44 @@ class ArithmeticService extends Actor {
 trait Position
 case object Left extends Position
 case object Right extends Position
-case class Result(value: Int, position: Position)
+case class Result(expr: Expression, value: Int, position: Position)
+
+class FlakynessException extends Exception("Flakyness")
 
 object FlakyExpressionCalculator {
   def apply(expr: Expression, position: Position): Props = Props(classOf[FlakyExpressionCalculator], expr, position)
 }
 
-class FlakynessException extends Exception("Flakyness")
-
+// This actor has the sole purpose of calculating a given expression and return the result to its parent. It takes
+// an additional argument, myPosition, which is used to signal the parent which side of its expression has been
+// calculated.
+// If the expression is a Const then this actor replies to its parent with an immediate answer. If the expression is
+// an arithmetic operation, this actor will create two children workers to first evaluate both sides of the operation
+// and then proceeds evaluating the original expression. For example if (3 + 4) * (11 + 1) is to be evaluated then
+// (3 + 4) is passed to one child worker, and (11 + 1) is to the other. After they return 7 and 12 (in any order), the
+// final value of 7 * 12 = 84 is delivered to the parent.
 class FlakyExpressionCalculator(val expr: Expression, val myPosition: Position) extends Actor {
-  override val supervisorStrategy = OneForOneStrategy() {
-    case _: FlakynessException => Restart
-    case _: ArithmeticException => Escalate
+  val log = Logging(context.system, this)
+  // Similar to the supervisor strategy of the ArithmeticService but when an ArithmeticException is encountered
+  // it is escalated to the parent. The parent of this actor is either another FlakyExpressionCalculator or the
+  // ArithmeticService. Since the calculators all escalate, no matter how deep the exception happened, the
+  // ArithmeticService will decide on the fate of the job (in our case, stop it).
+  override val supervisorStrategy = OneForOneStrategy(loggingEnabled = false) {
+    case _: FlakynessException =>
+      log.warning("Evaluation of {} failed, restarting.", expr)
+      Restart
+    case _: ArithmeticException =>
+      Escalate
   }
 
+  // The value of these variables will be reinitialized after every restart. The only stable data the actor has
+  // during restarts is those embedded in the Props when it was created. In this case expr, and myPosition.
   var results  = Map.empty[Position, Int]
   var expected = Set[Position](Left, Right)
 
   override def preStart(): Unit = expr match {
     case Const(value) =>
-      context.parent ! Result(value, myPosition)
+      context.parent ! Result(expr, value, myPosition)
       expected = Set.empty
     case _ =>
       context.actorOf(FlakyExpressionCalculator(expr.left, Left), name = "left")
@@ -66,13 +115,17 @@ class FlakyExpressionCalculator(val expr: Expression, val myPosition: Position) 
   }
 
   def receive = {
-    case Result(value, position) if expected(position) =>
-      flakyness()
+    case Result(_, value, position) if expected(position) =>
       expected -= position
       results += position -> value
-      if (results.size == 2)
-        context.parent ! Result(evaluate(expr, results(Left), results(Right)), myPosition)
-    case Result(_, position) =>
+      if (results.size == 2) {
+        // Sometimes we fail to calculate
+        flakyness()
+        val result: Int = evaluate(expr, results(Left), results(Right))
+        log.info("Evaluated expression {} to value {}", expr, result)
+        context.parent ! Result(expr, result, myPosition)
+      }
+    case Result(_, _, position) =>
       throw new IllegalStateException(s"Expected results for positions ${expected.mkString(", ")} but got position $position")
   }
 
@@ -88,6 +141,7 @@ class FlakyExpressionCalculator(val expr: Expression, val myPosition: Position) 
 
 }
 
+// DUMMY MAIN, WILL BE REMOVED
 object Main {
   def main(args: Array[String]) {
     val system = ActorSystem("calculator-system")
